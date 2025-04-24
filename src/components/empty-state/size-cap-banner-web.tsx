6e48433a2cdf2a9fb26df622c9f34d1c4c094216
Alex Pineda <alexpineda@fastmail.com>
2025-04-24 14:06:39 -0400

    fix download url

--- src/components/empty-state/size-cap-banner-web.tsx
+++ src/components/empty-state/size-cap-banner-web.tsx
@@ -32,7 +32,7 @@ export function SizeCapBanner({
               </span>
               .&nbsp;
               <a
-                href="https://reposnap.io/desktop"
+                href="https://www.reposnap.io/download"
                 className="underline underline-offset-2 hover:text-orange-50"
               >
                 Grab the desktop app for unlimited size →
